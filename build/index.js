c1c77e307b79779a3c388b7aeb983de92961639e
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-03-13 17:57:15 +0300

    sort algorithm tutorial

--- build/index.js
+++ build/index.js
@@ -1,11 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
-// import {NumbersCollection} from "./NumbersCollection";
+const NumbersCollection_1 = require("./NumbersCollection");
 const CharactersCollection_1 = require("./CharactersCollection");
-// const numbersCollection = new NumbersCollection([2, 4, -1, -4, -6, 9, -8])
-const charactersCollection = new CharactersCollection_1.CharactersCollection('aaaABbBCxyZz');
-// const sorter = new Sorter(numbersCollection)
-const sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
+const LinkedList_1 = require("./LinkedList");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([2, 4, -1, -4, -6, 9, -8]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('XaaBbxejrnrwK');
 console.log(charactersCollection.data);
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(22);
+linkedList.add(1);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-import {Sorter} from "./Sorter";
-// import {NumbersCollection} from "./NumbersCollection";
+import {NumbersCollection} from "./NumbersCollection";
 import {CharactersCollection} from "./CharactersCollection";
+import {LinkedList} from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([2, 4, -1, -4, -6, 9, -8])
-const charactersCollection = new CharactersCollection('aaaABbBCxyZz')
+const numbersCollection = new NumbersCollection([2, 4, -1, -4, -6, 9, -8])
+numbersCollection.sort()
+console.log(numbersCollection.data)
 
-// const sorter = new Sorter(numbersCollection)
-const sorter = new Sorter(charactersCollection)
-
-sorter.sort()
+const charactersCollection = new CharactersCollection('XaaBbxejrnrwK')
 console.log(charactersCollection.data)
+
+
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(22)
+linkedList.add(1)
+linkedList.add(4)
+
+linkedList.sort()
+linkedList.print()
+
+
+
+
+
+
 
